a34a1eb8b9e5b111ade08f497c09472da50b2f1f
xFatal1tYx <olkosarev@gmail.com>
2022-11-02 22:52:10 +0300

    Minor bug fixes

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -9,16 +9,6 @@ public class User {
     @Id
     private Long id;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
     @Column
     private String name;
 
@@ -36,6 +26,16 @@ public class User {
         this.name = name;
         this.lastName = lastName;
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
     }
 
     public Long getId() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -24,7 +24,5 @@ public class Util {
         }
         return connection;
     }
-
-
 }
 
